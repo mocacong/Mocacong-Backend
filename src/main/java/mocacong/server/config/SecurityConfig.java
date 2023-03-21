8b9785b5ee2036e5518819d2b47ca9aa274bb130
kth990303 <kth990303@naver.com>
2023-03-21 17:30:21 +0900

    style: SecurityConfig 클래스 띄어쓰기 컨벤션 적용

--- src/main/java/mocacong/server/config/SecurityConfig.java
+++ src/main/java/mocacong/server/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
