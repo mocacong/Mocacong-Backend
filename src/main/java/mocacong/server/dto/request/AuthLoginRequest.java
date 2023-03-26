2045dfae9ad757e9d2f2ffe87869554da1bc9dcc
kth990303 <kth990303@naver.com>
2023-03-26 21:46:17 +0900

    feat: 예외 반환 dto를 받는 controller advice 구현

--- src/main/java/mocacong/server/dto/request/AuthLoginRequest.java
+++ src/main/java/mocacong/server/dto/request/AuthLoginRequest.java
@@ -1,21 +1,21 @@
 package mocacong.server.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class AuthLoginRequest {
 
-    @Email
-    @NotBlank
+    @Email(message = "1006:이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "1012:공백일 수 없습니다.")
     private String email;
-    @NotBlank
+
+    @NotBlank(message = "1012:공백일 수 없습니다.")
     private String password;
 }
--- src/test/java/mocacong/server/acceptance/MemberAcceptanceTest.java
+++ src/test/java/mocacong/server/acceptance/MemberAcceptanceTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import mocacong.server.dto.request.MemberSignUpRequest;
+import mocacong.server.dto.response.ErrorResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,5 +28,22 @@ public class MemberAcceptanceTest extends AcceptanceTest {
                 .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("회원은 올바르지 않은 형식의 필드로 가입할 수 없다")
+    void signUpInvalidInputField() {
+        MemberSignUpRequest request = new MemberSignUpRequest("kth990303@naver.com", "abcdefgh", "케이", "010-1234-5678");
+
+        ErrorResponse response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract()
+                .as(ErrorResponse.class);
+
+        assertThat(response.getCode()).isEqualTo(1007);
     }
 }
