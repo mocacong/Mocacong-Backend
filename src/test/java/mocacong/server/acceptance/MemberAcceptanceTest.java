352b85d22f748795ab89b7c2db7f61cdfee97614
dlawltn0708 <dlawotn3@naver.com>
2023-03-27 21:47:23 +0900

    style: delete 메서드 파라미터 타입 변경

--- src/test/java/mocacong/server/acceptance/MemberAcceptanceTest.java
+++ src/test/java/mocacong/server/acceptance/MemberAcceptanceTest.java
@@ -3,9 +3,7 @@ package mocacong.server.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import mocacong.server.dto.request.AuthLoginRequest;
 import mocacong.server.dto.request.MemberSignUpRequest;
-import mocacong.server.dto.response.TokenResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,35 +29,5 @@ public class MemberAcceptanceTest extends AcceptanceTest {
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
-
-    @Test
-    @DisplayName("회원이 정상적으로 탈퇴한다")
-    void delete() {
-        MemberSignUpRequest request = new MemberSignUpRequest("dlawotn3@naver.com", "1234", "메리", "010-1234-5678");
-        RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when().post("/members")
-                .then().log().all()
-                .extract();
-        AuthLoginRequest loginRequest = new AuthLoginRequest("dlawotn3@naver.com", "1234");
-        String token = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(loginRequest)
-                .when().post("/login")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .as(TokenResponse.class)
-                .getToken();
-
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .auth().oauth2(token)
-                .when().delete("/members")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
+    
 }
