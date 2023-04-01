3891dfe31b278ac90cc571d88790803e3b0548f0
dlawltn0708 <dlawotn3@naver.com>
2023-04-01 18:17:09 +0900

    style: 예외메시지 수정

--- src/main/java/mocacong/server/exception/notfound/NotFoundReviewException.java
+++ src/main/java/mocacong/server/exception/notfound/NotFoundReviewException.java
@@ -3,6 +3,6 @@ package mocacong.server.exception.notfound;
 public class NotFoundReviewException extends NotFoundException {
 
     public NotFoundReviewException() {
-        super("해당 카페에 기여한 평점이 존재하지 않습니다.", 2006);
+        super("존재하지 않는 리뷰입니다.", 3008);
     }
 }
