efcf688372b399af9152bba60943ce9d5b274f37
dlawltn0708 <dlawotn3@naver.com>
2023-04-01 15:48:40 +0900

    test: 카페 세부정보 수정 인수테스트 작성

--- src/test/java/mocacong/server/acceptance/CafeAcceptanceTest.java
+++ src/test/java/mocacong/server/acceptance/CafeAcceptanceTest.java
@@ -13,8 +13,10 @@ import org.springframework.http.MediaType;
 import static mocacong.server.acceptance.AcceptanceFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class CafeAcceptanceTest extends AcceptanceTest {
+public class CafeAcceptanceTest extends AcceptanceTest{
 
     @Test
     @DisplayName("카페를 정상적으로 등록한다")
@@ -97,5 +99,56 @@ public class CafeAcceptanceTest extends AcceptanceTest {
                 .when().post("/cafes/" + mapId)
                 .then().log().all()
                 .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("등록한 카페에 대한 리뷰를 수정한다")
+    void updateCafeReview() {
+        String mapId = "12332312";
+        카페_등록(new CafeRegisterRequest(mapId, "메리네 카페"));
+        MemberSignUpRequest signUpRequest = new MemberSignUpRequest("kth990303@naver.com", "a1b2c3d4", "케이", "010-1234-5678");
+        회원_가입(signUpRequest);
+        String token = 로그인_토큰_발급(signUpRequest.getEmail(), signUpRequest.getPassword());
+        CafeReviewRequest request1 = new CafeReviewRequest(4, "solo", "빵빵해요", "여유로워요",
+                "깨끗해요", "충분해요", "조용해요", "편해요");
+        카페_리뷰_작성(token, mapId, request1);
+        CafeReviewRequest request2 = new CafeReviewRequest(3, "solo", "빵빵해요", "여유로워요",
+                "깨끗해요", "충분해요", "조용해요", "불편해요");
+
+        CafeReviewResponse actual = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(token)
+                .body(request2)
+                .when().put("/cafes/" + mapId)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(CafeReviewResponse.class);
+
+        assertAll(
+                () -> assertThat(actual.getScore()).isEqualTo(3),
+                () -> assertThat(actual.getDesk()).isEqualTo("불편해요")
+        );
+    }
+
+    @Test
+    @DisplayName("등록하지 않은 카페에 대한 리뷰는 수정할 수 없다")
+    void updateCafeReviewNotFoundReview() {
+        String mapId = "12332312";
+        카페_등록(new CafeRegisterRequest(mapId, "메리네 카페"));
+        MemberSignUpRequest signUpRequest = new MemberSignUpRequest("kth990303@naver.com", "a1b2c3d4", "케이", "010-1234-5678");
+        회원_가입(signUpRequest);
+        String token = 로그인_토큰_발급(signUpRequest.getEmail(), signUpRequest.getPassword());
+        CafeReviewRequest request = new CafeReviewRequest(3, "solo", "빵빵해요", "여유로워요",
+                "깨끗해요", "충분해요", "조용해요", "불편해요");
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(token)
+                .body(request)
+                .when().put("/cafes/" + mapId)
+                .then().log().all()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body("code", equalTo(2006));
     }
 }
