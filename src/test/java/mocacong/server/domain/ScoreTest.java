99d79144f9dc9f60012dc36cd91db8221225264b
dlawltn0708 <dlawotn3@naver.com>
2023-04-02 20:32:16 +0900

    test: 수정 메서드 검증 테스트 추가

--- src/test/java/mocacong/server/domain/ScoreTest.java
+++ src/test/java/mocacong/server/domain/ScoreTest.java
@@ -19,4 +19,15 @@ class ScoreTest {
         assertThatThrownBy(() -> new Score(score, member, cafe))
                 .isInstanceOf(InvalidScoreException.class);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 6})
+    @DisplayName("수정 시 평점이 1점 이상 5점 이하가 아니면 예외를 반환한다")
+    void updateInvalidRangeScore(int score) {
+        Member member = new Member("kth@naver.com", "a1b2c3d4", "케이", "010-1234-5678");
+        Cafe cafe = new Cafe("1", "케이카페");
+        Score oldScore = new Score(3, member, cafe);
+        assertThatThrownBy(() -> oldScore.updateScore(score))
+                .isInstanceOf(InvalidScoreException.class);
+    }
 }
--- src/test/java/mocacong/server/domain/StudyTypeTest.java
+++ src/test/java/mocacong/server/domain/StudyTypeTest.java
@@ -1,10 +1,11 @@
 package mocacong.server.domain;
 
 import mocacong.server.exception.badrequest.InvalidStudyTypeException;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StudyTypeTest {
 
@@ -24,6 +25,18 @@ class StudyTypeTest {
         Cafe cafe = new Cafe("1", "케이카페");
 
         StudyType studyType = new StudyType(member, cafe, "SoLo");
+
+        assertThat(studyType.getStudyTypeValue()).isEqualTo("solo");
+    }
+
+    @Test
+    @DisplayName("수정 시 타입에 대문자가 들어오더라도 소문자로 처리돼서 저장된다")
+    void updateStudyTypeToLowerCase() {
+        Member member = new Member("kth@naver.com", "a1b2c3d4", "케이", "010-1234-5678");
+        Cafe cafe = new Cafe("1", "케이카페");
+
+        StudyType studyType = new StudyType(member, cafe, "SoLo");
+        studyType.updateStudyTypeValue("SoLO");
 
         assertThat(studyType.getStudyTypeValue()).isEqualTo("solo");
     }
