7e2b957ef44bcd6c25433ade4d743bf9ec3312ef
kth990303 <kth990303@naver.com>
2023-04-09 16:57:52 +0900

    feat: apple oauth 로그인 시 platform id 반환 기능 구현

---
+++ src/main/java/mocacong/server/security/auth/apple/ApplePlatformMemberResponse.java
@@ -0,0 +1,14 @@
+package mocacong.server.security.auth.apple;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ApplePlatformMemberResponse {
+
+    private String platformId;
+}
