968c490bd27dff88e0bf8dd4e94a8d5d7e4fc341
kth990303 <kth990303@naver.com>
2023-04-11 11:56:39 +0900

    refact: 불필요한 메서드 및 `@Setter` 삭제

--- src/main/java/mocacong/server/domain/BaseTime.java
+++ src/main/java/mocacong/server/domain/BaseTime.java
@@ -1,16 +1,15 @@
 package mocacong.server.domain;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
 @Getter
-@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTime {
@@ -22,16 +21,4 @@ public class BaseTime {
     @LastModifiedDate
     @Column(name = "modified_time")
     private LocalDateTime modifiedTime;
-
-    @PrePersist
-    public void before() {
-        LocalDateTime now = LocalDateTime.now();
-        this.createdTime = now;
-        this.modifiedTime = now;
-    }
-
-    @PreUpdate
-    public void always() {
-        this.modifiedTime = LocalDateTime.now();
-    }
 }
