987648b1b5798c92a6729ba3b17efa57d21c8917
kth990303 <kth990303@naver.com>
2023-04-19 12:03:26 +0900

    feat: StudyType 엔티티 삭제 후 enum 으로 변경

---
+++ src/main/java/mocacong/server/domain/cafedetail/StudyType.java
@@ -0,0 +1,27 @@
+package mocacong.server.domain.cafedetail;
+
+import java.util.Arrays;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mocacong.server.exception.badrequest.InvalidStudyTypeException;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public enum StudyType {
+
+    SOLO("solo"),
+    GROUP("group"),
+    BOTH("both");
+
+    private String value;
+
+    public static StudyType from(String value) {
+        return Arrays.stream(values())
+                .filter(it -> Objects.equals(it.value, value))
+                .findFirst()
+                .orElseThrow(InvalidStudyTypeException::new);
+    }
+}
