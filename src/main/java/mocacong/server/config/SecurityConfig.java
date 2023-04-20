7b4f27d3f80443c549528521dc650a67bb0284e5
kth990303 <kth990303@naver.com>
2023-04-20 19:12:21 +0900

    feat: CORS 이슈 해결 적용

--- src/main/java/mocacong/server/config/SecurityConfig.java
+++ src/main/java/mocacong/server/config/SecurityConfig.java
@@ -15,7 +15,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
                 .headers().frameOptions().disable();
