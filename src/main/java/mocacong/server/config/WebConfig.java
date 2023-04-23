0950c5bcd731418906936780f9da1fa980bdaee8
TaeHyeon Kim <kth990303@naver.com>
2023-04-23 10:21:45 +0900

    feat: CORS 이슈 해결 적용 (#40)

---
+++ src/main/java/mocacong/server/config/WebConfig.java
@@ -0,0 +1,23 @@
+package mocacong.server.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION)
+                .maxAge(5000);
+    }
+}
