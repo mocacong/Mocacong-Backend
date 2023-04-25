a58636ec3954f082cdc17549558bbe0a2bf35d05
kth990303 <kth990303@naver.com>
2023-04-25 21:30:05 +0900

    feat: kakao oauth 유저 정보 발급 기능 구현

---
+++ src/main/java/mocacong/server/security/auth/kakao/KakaoUserClient.java
@@ -0,0 +1,14 @@
+package mocacong.server.security.auth.kakao;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "kakao-user-client", url = "https://kapi.kakao.com/v2/user/me")
+public interface KakaoUserClient {
+
+    @GetMapping
+    KakaoUser getUser(@SpringQueryMap KakaoUserRequest request,
+                      @RequestHeader(name = "Authorization") String authorization);
+}
