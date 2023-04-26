a10901e519f25ccf06686f1886e2d31769480175
dlawltn0708 <dlawotn3@naver.com>
2023-04-26 10:41:07 +0900

    refact: 스프링 시큐리티 의존성 제거

--- src/main/java/mocacong/server/config/SecurityConfig.java
+++ src/main/java/mocacong/server/config/SecurityConfig.java
@@ -3,24 +3,10 @@ package mocacong.server.config;
 import mocacong.server.security.EncryptUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .formLogin().disable()
-                .headers().frameOptions().disable();
-        return http.build();
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new PasswordEncoder() {
