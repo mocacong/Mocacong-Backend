f0bdff1adac7b533bc4e156033f583d2a0f5f472
TaeHyeon Kim <kth990303@naver.com>
2023-04-28 07:20:51 +0900

    feat: 카페 코멘트 목록 조회 api 구현 (#48)
    * fix: 카페 조회 화면에서 댓글은 최대 3개까지만 보이도록 변경
    
    * feat: 카페 코멘트 목록 조회 비즈니스 로직 구현
    
    * feat: 카페 코멘트 목록 조회 api 구현

--- src/main/java/mocacong/server/repository/CommentRepository.java
+++ src/main/java/mocacong/server/repository/CommentRepository.java
@@ -2,9 +2,13 @@ package mocacong.server.repository;
 
 import java.util.List;
 import mocacong.server.domain.Comment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByMemberId(Long memberId);
+
+    Slice<Comment> findAllByCafeId(Long cafeId, Pageable pageRequest);
 }
