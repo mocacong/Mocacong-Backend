14f08b42b3909e6f5ecf9f425d7f0b1724637cec
dlawltn0708 <dlawotn3@naver.com>
2023-05-02 15:50:09 +0900

    refact: DatabaseCleaner에서 cafe_image 테이블 격리 작업 추가

--- src/test/java/mocacong/server/service/CafeServiceTest.java
+++ src/test/java/mocacong/server/service/CafeServiceTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.when;
 
 @ServiceTest
@@ -483,11 +482,10 @@ class CafeServiceTest {
 
         when(awsS3Uploader.uploadImage(mockMultipartFile)).thenReturn("test_img.jpg");
         cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
-        //cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
+        cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
 
         Cafe actual = cafeRepository.findByMapId(mapId).orElseThrow(NotFoundCafeException::new);
         assertAll(
-                () -> assertDoesNotThrow(() -> cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile)),
                 () -> assertThat(actual.getCafeImages()).hasSize(2),
                 () -> assertThat(actual.getCafeImages().get(0).getImgUrl()).isEqualTo(expected),
                 () -> assertThat(actual.getCafeImages().get(1).getImgUrl()).isEqualTo(expected)
@@ -506,40 +504,42 @@ class CafeServiceTest {
         FileInputStream fileInputStream = new FileInputStream("src/test/resources/images/" + expected);
         MockMultipartFile mockMultipartFile = new MockMultipartFile("test_img", expected, "jpg", fileInputStream);
         when(awsS3Uploader.uploadImage(mockMultipartFile)).thenReturn("test_img.jpg");
+        cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
+        cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
+        cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
 
         CafeImageResponse actual = cafeService.findCafeImages("dlawotn3@naver.com", mapId, 0, 3);
 
         List<CafeImage> cafeImages = actual.getCafeImages();
         assertAll(
-                () -> assertDoesNotThrow(() -> cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile)),
-                () -> assertDoesNotThrow(() -> cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile)),
-                () -> assertDoesNotThrow(() -> cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile)),
                 () -> assertThat(actual.getCurrentPage()).isEqualTo(0),
                 () -> assertThat(cafeImages).hasSize(3)
         );
     }
-//
-//    @Test
-//    @DisplayName("카페를 조회할 때 이미지는 10개까지만 보여준다")
-//    void findCafeAndShowLimitImages() throws IOException{
-//        String expected = "test_img.jpg";
-//        Member member = new Member("dlawotn3@naver.com", "encodePassword", "케이", "010-1234-5678");
-//        memberRepository.save(member);
-//        Cafe cafe = new Cafe("2143154352323", "케이카페");
-//        cafeRepository.save(cafe);
-//        String mapId = cafe.getMapId();
-//        FileInputStream fileInputStream = new FileInputStream("src/test/resources/images/" + expected);
-//        MockMultipartFile mockMultipartFile = new MockMultipartFile("test_img", expected, "jpg", fileInputStream);
-//        when(awsS3Uploader.uploadImage(mockMultipartFile)).thenReturn("test_img.jpg");
-//        cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
-//
-//        Cafe actualCafe = cafeRepository.findByMapId(mapId).orElseThrow(NotFoundCafeException::new);
-//        CafeImageResponse actual = cafeService.findCafeImages("dlawotn3@naver.com", mapId, 0, 10);
-//
-//        List<CafeImage> cafeImages = actual.getCafeImages();
-//        assertAll(
-//                () -> assertThat(cafeImages).hasSize(10),
-//                () -> assertThat(actualCafe.getCafeImages()).hasSize(11)
-//        );
-//    }
+
+    @Test
+    @DisplayName("카페를 조회할 때 이미지는 10개까지만 보여준다")
+    void findCafeAndShowLimitImages() throws IOException{
+        String expected = "test_img.jpg";
+        Member member = new Member("dlawotn3@naver.com", "encodePassword", "케이", "010-1234-5678");
+        memberRepository.save(member);
+        Cafe cafe = new Cafe("2143154352323", "케이카페");
+        cafeRepository.save(cafe);
+        String mapId = cafe.getMapId();
+        FileInputStream fileInputStream = new FileInputStream("src/test/resources/images/" + expected);
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("test_img", expected, "jpg", fileInputStream);
+        when(awsS3Uploader.uploadImage(mockMultipartFile)).thenReturn("test_img.jpg");
+        for (int i = 0; i < 11; i++) {
+            cafeService.saveCafeImage(member.getEmail(), mapId, mockMultipartFile);
+        }
+
+        Cafe actualCafe = cafeRepository.findByMapId(mapId).orElseThrow(NotFoundCafeException::new);
+        CafeImageResponse actual = cafeService.findCafeImages("dlawotn3@naver.com", mapId, 0, 10);
+
+        List<CafeImage> cafeImages = actual.getCafeImages();
+        assertAll(
+                () -> assertThat(cafeImages).hasSize(10),
+                () -> assertThat(actualCafe.getCafeImages()).hasSize(11)
+        );
+    }
 }
--- src/test/java/mocacong/server/support/DatabaseCleaner.java
+++ src/test/java/mocacong/server/support/DatabaseCleaner.java
@@ -1,14 +1,15 @@
 package mocacong.server.support;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Table;
 import javax.persistence.metamodel.Type;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class DatabaseCleaner {
@@ -16,6 +17,7 @@ public class DatabaseCleaner {
     private static final String FOREIGN_KEY_RULE_UPDATE_FORMAT = "SET REFERENTIAL_INTEGRITY %s";
     private static final String TRUNCATE_FORMAT = "TRUNCATE TABLE %s";
     private static final String ID_RESET_FORMAT = "ALTER TABLE %s ALTER COLUMN %s_id RESTART WITH 1";
+    private static final String TRUNCATE_CAFE_IMAGE_TABLE = "TRUNCATE TABLE cafe_image";
 
     @PersistenceContext
     EntityManager entityManager;
@@ -41,6 +43,7 @@ public class DatabaseCleaner {
         for (final String tableName : tableNames) {
             entityManager.createNativeQuery(String.format(TRUNCATE_FORMAT, tableName)).executeUpdate();
             entityManager.createNativeQuery(String.format(ID_RESET_FORMAT, tableName, tableName)).executeUpdate();
+            entityManager.createNativeQuery(TRUNCATE_CAFE_IMAGE_TABLE).executeUpdate();
         }
         entityManager.createNativeQuery(String.format(FOREIGN_KEY_RULE_UPDATE_FORMAT, "TRUE"))
                 .executeUpdate();
