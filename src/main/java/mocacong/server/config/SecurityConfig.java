8838ac6bce78996ddd48d8738906321cd4d2ba22
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-05-07 22:39:31 +0900

    fix: SpringSecurity 인증 더블 체크 버그 수정 (#46)
    * fix: SpringSecurity 인증 더블 체크 버그 수정
    
    * refact: 스프링 시큐리티 의존성 제거
    
    ---------
    
    Co-authored-by: kth990303 <kth990303@naver.com>

--- src/main/java/mocacong/server/config/SecurityConfig.java
+++ src/main/java/mocacong/server/config/SecurityConfig.java
@@ -1,28 +1,24 @@
 package mocacong.server.config;
 
+import mocacong.server.security.EncryptUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .formLogin().disable()
-                .headers().frameOptions().disable();
-        return http.build();
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return EncryptUtils.encrypt(rawPassword.toString());
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return encodedPassword.equals(EncryptUtils.encrypt(rawPassword.toString()));
+            }
+        };
     }
 }
