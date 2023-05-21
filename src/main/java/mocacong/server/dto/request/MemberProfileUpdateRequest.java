7ef5ffe52bb672d2f977e9c769d879b30934b06b
dlawltn0708 <dlawotn3@naver.com>
2023-05-21 23:45:06 +0900

    fix: MemberProfileUpdateRequest DTO 변경

--- src/main/java/mocacong/server/dto/request/MemberProfileUpdateRequest.java
+++ src/main/java/mocacong/server/dto/request/MemberProfileUpdateRequest.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,11 +13,12 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class MemberProfileUpdateRequest {
 
+    @Email(message = "1006:이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "1012:공백일 수 없습니다.")
-    private String nickname;
+    private String email;
 
     @NotBlank(message = "1012:공백일 수 없습니다.")
-    private String password;
+    private String nickname;
 
     @NotBlank(message = "1012:공백일 수 없습니다.")
     private String phone;
