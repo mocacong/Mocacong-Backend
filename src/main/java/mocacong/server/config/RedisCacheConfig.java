9bdc28dbe08ef65bc9366011def1a2a9007cdb2a
kth990303 <kth990303@naver.com>
2023-05-23 00:26:15 +0900

    refactor: 인증 캐시 ttl 변경 및 주석 추가

--- src/main/java/mocacong/server/config/RedisCacheConfig.java
+++ src/main/java/mocacong/server/config/RedisCacheConfig.java
@@ -18,14 +18,34 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisCacheConfig {
 
+    private static final long DELTA_TO_AVOID_CONCURRENCY_TIME = 60000L;
+
     @Value("${security.jwt.token.expire-length}")
     private long accessTokenValidityInMilliseconds;
 
     @Bean
-    public CacheManager oauthPublicKeyCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        /* public key 갱신은 1년에 몇 번 안되므로 ttl 1주일로 설정 */
+    @Primary
+    public CacheManager cafeCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        /*
+         * 카페 관련 캐시는 충분히 많이 쌓일 수 있으므로 OOM 방지 차 ttl 12시간으로 설정
+         */
         RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
-                .entryTtl(Duration.ofDays(7L));
+                .entryTtl(Duration.ofHours(12L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+    @Bean
+    public CacheManager oauthPublicKeyCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        /*
+         * public key 갱신은 1년에 몇 번 안되므로 ttl 3일로 설정
+         * 유저가 하루 1번 로그인한다고 가정, 최소 1일은 넘기는 것이 좋다고 판단
+         */
+        RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
+                .entryTtl(Duration.ofDays(3L));
         return RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory)
                 .cacheDefaults(redisCacheConfiguration)
@@ -34,22 +54,12 @@ public class RedisCacheConfig {
 
     @Bean
     public CacheManager accessTokenCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        /* accessToken 시간만큼 ttl 설정 */
+        /*
+         * accessToken 시간만큼 ttl 설정하되,
+         * 만료 직전 캐시 조회하여 로그인 안되는 동시성 이슈 방지를 위해 accessToken ttl 보다 1분 일찍 만료
+         */
         RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
-                .entryTtl(Duration.ofMillis(accessTokenValidityInMilliseconds));
-
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-    }
-
-    @Bean
-    @Primary
-    public CacheManager cafeCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        /* 카페 관련 캐시는 충분히 많이 쌓일 수 있으므로 OOM 방지 차 ttl 12시간으로 설정 */
-        RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
-                .entryTtl(Duration.ofHours(12L));
+                .entryTtl(Duration.ofMillis(accessTokenValidityInMilliseconds - DELTA_TO_AVOID_CONCURRENCY_TIME));
 
         return RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory)
