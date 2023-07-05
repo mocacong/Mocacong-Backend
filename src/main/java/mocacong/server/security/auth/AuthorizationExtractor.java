544a01cf59d89fafee28a85f9f89d19a87ec7cd2
JiSoo Lim <imjisu@imjisuui-MacBookPro.local>
2023-07-05 16:49:26 +0900

    feat: 토큰 공백 예외 처리 로직 추가

--- src/main/java/mocacong/server/security/auth/AuthorizationExtractor.java
+++ src/main/java/mocacong/server/security/auth/AuthorizationExtractor.java
@@ -1,6 +1,7 @@
 package mocacong.server.security.auth;
 
 import lombok.NoArgsConstructor;
+import mocacong.server.exception.badrequest.BlankTokenException;
 import mocacong.server.exception.unauthorized.InvalidBearerException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,12 @@ public class AuthorizationExtractor {
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
             if (value.toLowerCase().startsWith(AUTHENTICATION_TYPE.toLowerCase())) {
-                return value.split(" ")[TOKEN_INDEX];
+                String parts[] = value.split(" ");
+                if (parts.length > 1) {
+                    return value.split(" ")[TOKEN_INDEX];
+                } else {
+                    throw new BlankTokenException();
+                }
             }
         }
         throw new InvalidBearerException();
