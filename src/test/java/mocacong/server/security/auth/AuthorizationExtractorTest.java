998fa1bc881beb3203677ca53979e06670091978
JiSoo Lim <imjisu@imjisuui-MacBookPro.local>
2023-07-05 17:13:11 +0900

    test: token 공백 예외 반환 테스트코드 추가

---
+++ src/test/java/mocacong/server/security/auth/AuthorizationExtractorTest.java
@@ -0,0 +1,25 @@
+package mocacong.server.security.auth;
+
+import mocacong.server.exception.badrequest.BlankTokenException;
+import mocacong.server.exception.unauthorized.TokenExpiredException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorizationExtractorTest {
+
+    @DisplayName("공백 값으로 access token 추출을 시도하면 예외를 반환한다")
+    @Test
+    void extractAccessTokenByBlankToken() {
+        String token = "Bearer ";
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", token);
+
+        assertThatThrownBy(() -> AuthorizationExtractor.extractAccessToken(request))
+                .isInstanceOf(BlankTokenException.class);
+    }
+}
