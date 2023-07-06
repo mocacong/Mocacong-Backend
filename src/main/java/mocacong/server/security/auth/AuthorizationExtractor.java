b9d70c76746c495a8f57479458dcbcf98fd5a914
JiSoo Lim <imjisu@imjisuui-MacBookPro.local>
2023-07-06 11:40:34 +0900

    fix: 토큰 공백 검증 로직 수정

--- src/main/java/mocacong/server/security/auth/AuthorizationExtractor.java
+++ src/main/java/mocacong/server/security/auth/AuthorizationExtractor.java
@@ -12,23 +12,23 @@ public class AuthorizationExtractor {
 
     private static final String AUTHENTICATION_TYPE = "Bearer";
     private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
-    private static final int TOKEN_INDEX = 1;
+    private static final int START_TOKEN_INDEX = 6;
 
     public static String extractAccessToken(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaders(AUTHORIZATION_HEADER_KEY);
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
             if (value.toLowerCase().startsWith(AUTHENTICATION_TYPE.toLowerCase())) {
-                String[] parts = value.split(" ");
-                validateParts(parts);
-                return parts[TOKEN_INDEX];
+                String extractToken = value.trim().substring(START_TOKEN_INDEX);
+                validateExtractToken(extractToken);
+                return extractToken;
             }
         }
         throw new InvalidBearerException();
     }
 
-    private static void validateParts(String[] parts) {
-        if (parts.length <= 1) {
+    private static void validateExtractToken(String extractToken) {
+        if (extractToken.isBlank()) {
             throw new BlankTokenException();
         }
     }
