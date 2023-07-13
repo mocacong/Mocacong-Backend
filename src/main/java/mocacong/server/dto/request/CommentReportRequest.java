42d8bee571b52121fba088b4e4c429cf8e51678f
JiSoo Lim <imjisu@imjisuui-MacBookPro.local>
2023-07-14 01:18:10 +0900

    feat: 코멘트 신고 로직에 신고 사유 필드 추가

---
+++ src/main/java/mocacong/server/dto/request/CommentReportRequest.java
@@ -0,0 +1,25 @@
+package mocacong.server.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@ToString
+public class CafeReviewRequest {
+
+    @NotNull(message = "3009:공백일 수 없습니다.")
+    private Integer myScore;
+
+    @NotBlank(message = "3009:공백일 수 없습니다.")
+    private String myStudyType;
+
+    private String myWifi;
+    private String myParking;
+    private String myToilet;
+    private String myPower;
+    private String mySound;
+    private String myDesk;
+}
--- src/main/java/mocacong/server/dto/response/CommentReportResponse.java
+++ src/main/java/mocacong/server/dto/response/CommentReportResponse.java
@@ -1,16 +1,2 @@
-package mocacong.server.dto.response;
-
-import lombok.*;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@ToString
-public class CommentReportResponse {
-
-    private Long id;
-
-    private String reporterNickname;
-
-    private int reportCount;
+package mocacong.server.dto.response;public class CommentReportResponse {
 }
