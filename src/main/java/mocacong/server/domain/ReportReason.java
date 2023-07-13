e57b7579661039d7f52c8664512f0860bb5b79a1
JiSoo Lim <imjisu@imjisuui-MacBookPro.local>
2023-07-13 20:01:42 +0900

    feat: ReportReason Enum 추가

---
+++ src/main/java/mocacong/server/domain/ReportReason.java
@@ -0,0 +1,32 @@
+package mocacong.server.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mocacong.server.exception.badrequest.InvalidPlatformException;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public enum ReportReason {
+
+    FISHING_HARASSMENT_SPAM("fishing_harassment_spam"),
+    LEAKING_FRAUD("leaking_fraud"),
+    PORNOGRAPHY("pornography"),
+    INAPPROPRIATE_CONTENT("inappropriate_content"),
+    INSULT("insult"),
+    COMMERCIAL_AD("commercial_ad"),
+    POLITICAL_CONTENT("political_content");
+
+    private String value;
+
+    public static ReportReason from(String value) {
+        return Arrays.stream(values())
+                .filter(it -> Objects.equals(it.value, value))
+                .findFirst()
+                .orElseThrow(InvalidPlatformException::new);
+    }
+}
