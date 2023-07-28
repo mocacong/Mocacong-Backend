3b4f46acc6aef311bd37f43180a95ea3fba1b08c
임지수 <imjisu@imjisuui-MacBookPro.local>
2023-07-28 21:50:15 +0900

    feat: 액세스 토큰 재발급 관련 인수 테스트 코드 추가

--- src/test/java/mocacong/server/acceptance/AuthAcceptanceTest.java
+++ src/test/java/mocacong/server/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,7 @@
 package mocacong.server.acceptance;
 
 import io.restassured.RestAssured;
-import mocacong.server.dto.request.AppleLoginRequest;
-import mocacong.server.dto.request.AuthLoginRequest;
-import mocacong.server.dto.request.KakaoLoginRequest;
-import mocacong.server.dto.request.MemberSignUpRequest;
+import mocacong.server.dto.request.*;
 import mocacong.server.dto.response.OAuthTokenResponse;
 import mocacong.server.dto.response.TokenResponse;
 import mocacong.server.security.auth.OAuthPlatformMemberResponse;
@@ -16,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import static mocacong.server.acceptance.AcceptanceFixtures.회원_가입;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -97,5 +96,39 @@ public class AuthAcceptanceTest extends AcceptanceTest {
                 .as(OAuthTokenResponse.class);
 
         assertThat(actual.getEmail()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("회원이 정상적으로 Access Token을 재발급받는다")
+    void reissueAccessToken() {
+        MemberSignUpRequest signUpRequest = new MemberSignUpRequest("dlawotn3@naver.com", "a1b2c3d4", "메리");
+        AuthLoginRequest loginRequest = new AuthLoginRequest("dlawotn3@naver.com", "a1b2c3d4");
+        회원_가입(signUpRequest);
+        TokenResponse tokenResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(loginRequest)
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(TokenResponse.class);
+
+        String refreshToken = tokenResponse.getRefreshToken();
+        RefreshTokenRequest request = new RefreshTokenRequest(refreshToken);
+
+        TokenResponse response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/login/refresh")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(TokenResponse.class);
+
+        assertAll(
+                () -> assertNotNull(response.getAccessToken()),
+                () -> assertNotNull(response.getRefreshToken()),
+                () -> assertThat(response.getRefreshToken()).isEqualTo(refreshToken)
+        );
     }
 }
