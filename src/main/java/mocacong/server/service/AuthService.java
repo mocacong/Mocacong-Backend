d66a878c54624e5bbe480bddc41f12c16af69354
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-11-13 18:17:35 +0900

    refact: Kakao OAuth 로직을 카카오 SDK에 맞춰 수정 (#135)
    * fix: KakaoLoginRequest 인자명 수정
    
    * delete: KakaoAccessToken 관련 파일 수정
    
    * fix: kakao oauth 통신 로직 수정
    
    * delete: 이전 로직의 kakao test 파일 삭제
    
    * delete: 카카오 관련 환경변수 삭제
    
    * delete: 카카오 관련 환경변수 삭제
    
    * refact: 카카오 oauth 로그인 feign 통신 제거
    
    * delete: kakao feign client 관련 파일 제거
    
    * test: 카카오 oauth 로그인 로직 수정에 따른 테스트 코드 수정
    
    ---------
    
    Co-authored-by: dlawotn3 <imjisu@imjisuui-MacBookPro.local>

--- src/main/java/mocacong/server/service/AuthService.java
+++ src/main/java/mocacong/server/service/AuthService.java
@@ -16,7 +16,6 @@ import mocacong.server.repository.MemberRepository;
 import mocacong.server.security.auth.JwtTokenProvider;
 import mocacong.server.security.auth.OAuthPlatformMemberResponse;
 import mocacong.server.security.auth.apple.AppleOAuthUserProvider;
-import mocacong.server.security.auth.kakao.KakaoOAuthUserProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +27,6 @@ public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final AppleOAuthUserProvider appleOAuthUserProvider;
-    private final KakaoOAuthUserProvider kakaoOAuthUserProvider;
 
     public TokenResponse login(AuthLoginRequest request) {
         Member findMember = memberRepository.findByEmailAndPlatform(request.getEmail(), Platform.MOCACONG)
@@ -53,12 +51,11 @@ public class AuthService {
     }
 
     public OAuthTokenResponse kakaoOAuthLogin(KakaoLoginRequest request) {
-        OAuthPlatformMemberResponse kakaoPlatformMember =
-                kakaoOAuthUserProvider.getKakaoPlatformMember(request.getCode());
+
         return generateOAuthTokenResponse(
                 Platform.KAKAO,
-                kakaoPlatformMember.getEmail(),
-                kakaoPlatformMember.getPlatformId()
+                request.getEmail(),
+                request.getPlatformId()
         );
     }
 
