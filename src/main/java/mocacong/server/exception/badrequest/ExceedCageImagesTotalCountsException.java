722cd967172f24d25d2af1c9f9588edd7ef7379f
jisu <dlawotn3@naver.com>
2023-12-07 15:29:14 +0900

    style: modify ExceedCageImagesTotalCountsException message

--- src/main/java/mocacong/server/exception/badrequest/ExceedCageImagesTotalCountsException.java
+++ src/main/java/mocacong/server/exception/badrequest/ExceedCageImagesTotalCountsException.java
@@ -2,6 +2,6 @@ package mocacong.server.exception.badrequest;
 
 public class ExceedCageImagesTotalCountsException extends BadRequestException{
     public ExceedCageImagesTotalCountsException() {
-        super("카페 이미지는 3개 이상 등록하실 수 없습니다.", 2008);
+        super("카페 이미지는 4개 이상 등록하실 수 없습니다.", 2008);
     }
 }
