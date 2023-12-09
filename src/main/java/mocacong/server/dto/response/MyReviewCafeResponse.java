28925c997a85b5c324f25bc9ca75a3ac0e837722
JungWoo <rlawjddn103@naver.com>
2023-12-09 12:48:02 +0900

    feat: dto 변수명 수정

--- src/main/java/mocacong/server/dto/response/MyReviewCafeResponse.java
+++ src/main/java/mocacong/server/dto/response/MyReviewCafeResponse.java
@@ -14,12 +14,12 @@ public class MyReviewCafeResponse {
     private String myStudyType;
     private int myScore;
     private String roadAddress;
-    private String wifi;
-    private String parking;
-    private String toilet;
-    private String power;
-    private String sound;
-    private String desk;
+    private String myWifi;
+    private String myParking;
+    private String myToilet;
+    private String myPower;
+    private String mySound;
+    private String myDesk;
 
     public MyReviewCafeResponse(String mapId, String name, StudyType myStudyType, int myScore, String roadAddress, CafeDetail cafeDetail) {
         this.mapId = mapId;
@@ -27,11 +27,11 @@ public class MyReviewCafeResponse {
         this.myStudyType = myStudyType.getValue();
         this.myScore = myScore;
         this.roadAddress = roadAddress;
-        this.wifi = cafeDetail.getWifiValue();
-        this.parking = cafeDetail.getParkingValue();
-        this.toilet = cafeDetail.getToiletValue();
-        this.power = cafeDetail.getPowerValue();
-        this.sound = cafeDetail.getSoundValue();
-        this.desk = cafeDetail.getDeskValue();
+        this.myWifi = cafeDetail.getWifiValue();
+        this.myParking = cafeDetail.getParkingValue();
+        this.myToilet = cafeDetail.getToiletValue();
+        this.myPower = cafeDetail.getPowerValue();
+        this.mySound = cafeDetail.getSoundValue();
+        this.myDesk = cafeDetail.getDeskValue();
     }
 }
