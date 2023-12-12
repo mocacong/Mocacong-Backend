c4b58e08af1613c4cd12575ce915fd4b4332ec8e
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-12-12 21:40:01 +0900

    feat: change roadAddress and phoneNumber to nullable field in CafeRegisterRequest (#144)

--- src/main/java/mocacong/server/dto/request/CafeRegisterRequest.java
+++ src/main/java/mocacong/server/dto/request/CafeRegisterRequest.java
@@ -15,9 +15,7 @@ public class CafeRegisterRequest {
     @NotBlank(message = "1012:공백일 수 없습니다.")
     private String name;
 
-    @NotBlank(message = "1012:공백일 수 없습니다.")
     private String roadAddress;
 
-    @NotBlank(message = "1012:공백일 수 없습니다.")
     private String phoneNumber;
 }
